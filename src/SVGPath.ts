e5e8480aeaa7953655d4440659337d0d9347e43b
JOJO_Mac <Defrance06@gmail.com>
2018-03-14 14:54:27 +0100

    parse cubic bezier curve svg path relative and absolute coordinates

---
+++ src/SVGPath.ts
@@ -0,0 +1,122 @@
+import { vec2, vec3 } from "gl-matrix";
+import { CompoundPathComponent, IPathStyle } from "./CompoundPathComponent";
+import { CompoundPathEntityFactory } from "./CompoundPathEntityFactory";
+import { PathComponent, pathType } from "./PathComponent";
+export { SVGPathUtil };
+
+class SVGPathUtil {
+    constructor() { }
+    /**
+     * Create a CompoundEntity from a svg path
+     * @param entityId the enityId to give to the entity to be created
+     * @param svgPath the svgPath in string format
+     * @param compoundFactory the CompoundEntityFactory use for creating the compound entity
+     */
+    public parseSVGPath(entityId: number, svgPath: string, compoundFactory: CompoundPathEntityFactory): CompoundPathComponent {
+        // every c or C or M = a new cubic bezier path
+        // since we record 4 points for each cubic bezier we need record the last point of the previous path as the first of the new path
+        // once we have the 4 points coordinates
+        // create a new path
+        // at the end create a compoindPath which refer to the created path
+
+        const commands = svgPath.split(/(?=c|C|M|m)/g);
+        const points: vec2[] = [];
+        // Split the string so we have all elements in a array
+        const firstPathId = compoundFactory.pathEntityFactory.getLastPathId() + 1;
+        let nbPath = 0;
+        const referencePoint = vec2.create();
+        commands.forEach((command, index) => {
+            command = command.trim();
+            switch (command.charAt(0)) {
+                case "M":
+                    this.parseM(command, referencePoint, points);
+                    break;
+                case "C":
+                    if (index === 0) {
+                        return Error("A path can't begin with a C command");
+                    }
+                    this.parseC(command, referencePoint, points);
+                    createCubicBezierComponent(points);
+                    nbPath += 1;
+                    break;
+                case "c":
+                    this.parseC(command, referencePoint, points);
+                    createCubicBezierComponent(points);
+                    nbPath += 1;
+                    break;
+                default:
+                    return Error("Parsing of this command is not implemented : " + command);
+            }
+        });
+        // Remove the first poitns since we copy the previous points for every C command but the first command is a M command.
+        points.shift();
+
+        // create the compoundPath and add reference to the paths
+        function createCubicBezierComponent(pts: vec2[]) {
+            const id = compoundFactory.pathEntityFactory.getLastPathId() + 1;
+            const c = compoundFactory.pathEntityFactory.create(id, points.slice(-4), pathType.cubicBezier);
+            return c;
+        }
+
+        // create the compoundPath and add reference to the paths
+        const res = compoundFactory.create(entityId);
+        res.firstPathId = firstPathId;
+        res.nbPath = nbPath;
+        return res;
+    }
+
+    /** Parse a Move command,
+     *
+     * Set the point as a reference for next path,
+     * Point is pushed to the output array.
+     * @param c
+     * @param refVec
+     * @param output
+     */
+    public parseM(c: string, refVec: vec2, output: vec2[]) {
+        // remove 'M'
+        c = c.slice(1, c.length);
+        const coord = c.split(",");
+        const x = Number(coord[0]);
+        const y = Number(coord[1]);
+        if (x !== undefined && y !== undefined) {
+            output.push(vec2.fromValues(x, y));
+            vec2.copy(refVec, output[output.length - 1]);
+        } else {
+            throw Error("No readable coordinates on element : " + c);
+        }
+    }
+
+    /** Parse a cubic bezier curve,
+     * Set the last point as a reference for next path,
+     * Points are pushed to the output array.
+     * @param c
+     * @param refVec
+     * @param output
+     */
+    public parseC(c: string, refVec: vec2, output: vec2[]) {
+        const relative = c.charAt(0) === "c";
+        // remove 'C' or 'c'
+        c = c.slice(1, c.length);
+        const curvePts = c.split(/(?=,|\-)/g);
+        if (curvePts.length === 6) {
+            for (let i = 0; i < 6; i += 2) {
+                const x = Number(curvePts[i].replace(/^,/, ""));
+                const y = Number(curvePts[i + 1].replace(/^,/, ""));
+                if (relative) {
+                    output.push(vec2.fromValues(refVec[0] + x, refVec[1] + y));
+                } else {
+                    output.push(vec2.fromValues(x, y));
+                }
+                // last point would serve as a reference point for the next path
+                if (i === 4) {
+                    vec2.copy(refVec, output[output.length - 1]);
+                } else if (i === 0) {
+                    vec2.copy(refVec, output[output.length - 1]);
+                }
+            }
+        } else {
+            throw Error("C, Cubic bezier element need 3 points : " + c);
+        }
+    }
+}
---
+++ test/SVGPath.test.ts
@@ -0,0 +1,85 @@
+import { expect } from "chai";
+import { mat4, vec2 } from "gl-matrix";
+import "mocha";
+import { CompoundPathEntityFactory } from "../src/CompoundPathEntityFactory";
+import { SVGPathUtil } from "../src/SVGPath";
+
+describe("SVG path", () => {
+    // Path in absolute coordinates
+    const absPointPath1 = [vec2.fromValues(100, 200), vec2.fromValues(100, 100), vec2.fromValues(250, 100), vec2.fromValues(250, 200), vec2.fromValues(250, 300), vec2.fromValues(400, 300), vec2.fromValues(400, 200)];
+    const absSVGPath1 = "M100, 200 C100, 100, 250, 100, 250, 200 C250, 300, 400, 300, 400, 200";
+    const nbCurvePath1 = 2;
+
+    // Same path but in relative coordinate
+    // Path in relative coordinates
+    // First coordinate refer to the M command so it's the reference points for the next point
+    // First coordinate of a C command is relative to the last coordinte of the previous point
+    // subsequents coordinates are relative to the first point of their corresping command (ie: first point of a C command)
+    const relativePointPath1 = [vec2.fromValues(100, 200), vec2.fromValues(0, -100), vec2.fromValues(150, 0), vec2.fromValues(150, 100), vec2.fromValues(0, 100), vec2.fromValues(150, 0), vec2.fromValues(150, -100)];
+    const relativeSVGPath1  = "M100, 200 c0 -100, 150, 0, 150, 100 c0, 100, 150, 0, 150 -100";
+
+    let cPool: CompoundPathEntityFactory;
+    beforeEach(() => {
+        cPool = new CompoundPathEntityFactory(10, 100, 1000);
+    });
+    describe("parse a svg path composed of cubic bezier curve", ()  => {
+        it("absolute coordinate", () => {
+            expect(cPool.pathEntityFactory.pointPool.nbCreated).to.equal(0);
+            const svgUtil = new SVGPathUtil();
+            const res = svgUtil.parseSVGPath(1, absSVGPath1, cPool);
+
+            expect(res.nbPath).to.equal(nbCurvePath1);
+            const pointPool = cPool.pathEntityFactory.pointPool;
+            expect(pointPool.nbCreated).to.equal(nbCurvePath1 * 4);
+
+            // verify that for each cubier bezier curve we have 4 points (first correspond to the last of the previous one)
+            let j = 0;
+            for (let i = 0; i < pointPool.iterationLength; ++i) {
+                if (i % 4 === 0 && i !== 0) {
+                    j -= 1;
+                }
+                expect(pointPool.values[i].point[0]).to.equal(absPointPath1[j][0]);
+                expect(pointPool.values[i].point[1]).to.equal(absPointPath1[j][1]);
+                j += 1;
+            }
+        });
+        it("relative coordinates (should convert to absolute coordinates)", () => {
+            expect(cPool.pathEntityFactory.pointPool.nbCreated).to.equal(0);
+            const svgUtil = new SVGPathUtil();
+            const res = svgUtil.parseSVGPath(1, relativeSVGPath1, cPool);
+
+            expect(res.nbPath).to.equal(nbCurvePath1);
+            const pointPool = cPool.pathEntityFactory.pointPool;
+            expect(pointPool.nbCreated).to.equal(nbCurvePath1 * 4);
+
+            let j = 0;
+            for (let i = 0; i < pointPool.iterationLength; ++i) {
+                if (i % 4 === 0 && i !== 0) {
+                    j -= 1;
+                }
+                // should be converted to absolute coordinates
+                expect(pointPool.values[i].point[0]).to.equal(absPointPath1[j][0]);
+                expect(pointPool.values[i].point[1]).to.equal(absPointPath1[j][1]);
+                j += 1;
+            }
+
+        });
+    });
+});
+
+// convert absolute points to relative poitns
+    // let ref: vec2 = absPointPath1[0];
+    // relatibPointPath1.push(absPointPath1[0]);
+    // absPointPath1.forEach((p, i) => {
+    //     if ((i % 4 === 0 && i !== 0) || i === 1) {
+    //         // // make coordinate relative to the previous
+    //         const newP = vec2.create();
+    //         vec2.sub(newP, p, absPointPath1[i - 1]);
+    //         relatibPointPath1.push(newP);
+    //         ref = p;
+    //     } else if (i !== 0) {
+    //         const newP = vec2.create();
+    //         vec2.sub(newP, p, ref);
+    //         relatibPointPath1.push(newP);
+    //     }
+    // });
